852eb6350483e8b4114fb759fd699f14bc95c61c
wesleyhjpr <wesleyhjpr@gmail.com>
2023-12-21 15:13:35 -0300

    ..

--- tag-html-personalizado.js
+++ tag-html-personalizado.js
@@ -1,4 +1,4 @@
-class ListaNaoOrdenada extends HTMLElement {
+class ListaNaoOrdenadaSimples extends HTMLElement {
     connectedCallback() {
         this.innerHTML = 
         `<ul>
@@ -10,4 +10,49 @@ class ListaNaoOrdenada extends HTMLElement {
     }
 }
 
-customElements.define('lista-nao-ordenada', ListaNaoOrdenada);
+customElements.define('lista-nao-ordenada-simples', ListaNaoOrdenadaSimples);
+
+
+class ListaNaoOrdenadaComplexa extends HTMLElement {
+    static get observedAttributes() {
+        return [this._nomeDoAtributo];
+    }
+
+    constructor() {
+        super();
+        this._variavel = [];
+        this._nomeDoAtributo = 'meu-atributo-personalizado';
+    }
+
+    attributeChangedCallback(name, oldValue, newValue) {
+        if (name === this._nomeDoAtributo) {
+            this._variavel = JSON.parse(newValue);
+            this.renderList();
+        }
+    }
+
+    connectedCallback() {
+        this.style.color = "red";
+        const variavelAttribute = this.getAttribute(this._nomeDoAtributo);
+        if (variavelAttribute) {
+            this._variavel = JSON.parse(variavelAttribute);
+        } else {
+            this._variavel = [1, 2, 3];
+        }
+        this.renderList();
+    }
+
+    renderList() {
+        const ul = document.createElement('ul');
+        this._variavel.forEach(value => {
+            const li = document.createElement('li');
+            li.textContent = value;
+            ul.appendChild(li);
+        });
+        this.innerHTML = '';
+        this.appendChild(ul);
+    }
+}
+
+customElements.define('lista-nao-ordenada-complexa', ListaNaoOrdenadaComplexa);
+
